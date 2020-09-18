cb06aab58123f9c22223991c9a89bd140f72b810
anycodes <jiangyu.ly@alibaba-inc.com>
2020-09-18 15:56:32 +0800

    fix files

--- src/bootstrap.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const DEFAULTBOOTSTRAP = "#!/usr/bin/env bash\n\nexport PORT={port}\n{start}";
-export declare const DEFAULTPORT = 9000;
-export declare const DEFAULTSTART = "npx hexo server -p $PORT -s";
-//# sourceMappingURL=bootstrap.d.ts.map
